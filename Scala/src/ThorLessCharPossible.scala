05ebe444a3c1539a4fd4226d353e0de18756c418
Bertozzi Julien <bertozzijulien@gmail.com>
2015-09-24 23:04:41 +0200

    -("')

--- Scala/src/ThorLessCharPossible.scala
+++ Scala/src/ThorLessCharPossible.scala
@@ -1,7 +1,7 @@
 object Player extends App{var Array(x,y,a,b)=for(i<-readLine split " ")yield i.toInt
 while(true){b+=t(y-b,'S','N')
 a+=t(x-a,'E','W')
-println("")}
+println}
 def t(d:Int,y:Char,n:Char):Int={if(d>0){print(y)
 return 1}
 if(d<0){print(n)
