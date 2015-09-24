bfa9ad14a24c43e840d2708eae0983beac7abaff
Bertozzi Julien <bertozzijulien@gmail.com>
2015-09-24 21:25:11 +0200

    Update ThorLessCharPossible.scala
    236

--- Scala/src/ThorLessCharPossible.scala
+++ Scala/src/ThorLessCharPossible.scala
@@ -1,9 +1,9 @@
 object Player extends App{var Array(x,y,a,b)=for(i<-readLine split " ")yield i.toInt
-while(true){b+=t(y,b,"S","N")
-a+=t(x,a,"E","W")
+while(true){b+=t(y-b,'S','N')
+a+=t(x-a,'E','W')
 println("")}
-def t(l:Int,t:Int,y:String,n:String):Int={if(l>t){print(y)
+def t(d:Int,y:Char,n:Char):Int={if(d>0){print(y)
 return 1}
-if(l<t){print(n)
+if(d<0){print(n)
 return -1}
 0}}
