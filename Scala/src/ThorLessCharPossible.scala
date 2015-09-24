52666f9e40d138119d8d53332c3f525f226f7398
Bertozzi Julien <bertozzijulien@gmail.com>
2015-09-24 21:04:37 +0200

    Create ThorLessCharPossible.scala
    246 characters

---
+++ Scala/src/ThorLessCharPossible.scala
@@ -0,0 +1,9 @@
+object Player extends App{var Array(x,y,a,b)=for(i<-readLine split " ")yield i.toInt
+while(true){b+=t(y,b,"S","N")
+a+=t(x,a,"E","W")
+println("")}
+def t(l:Int,t:Int,y:String,n:String):Int={if(l>t){print(y)
+return 1}
+if(l<t){print(n)
+return -1}
+0}}
