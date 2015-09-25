6919f26cc956be39a46951c385004ffe474b7186
Bertozzi Julien <bertozzijulien@gmail.com>
2015-09-25 12:51:30 +0200

    223 chars

--- Scala/src/ThorLessCharPossible.scala
+++ Scala/src/ThorLessCharPossible.scala
@@ -1,6 +1,5 @@
 object Player extends App{var Array(x,y,a,b)=for(i<-readLine split " ")yield i.toInt
-while(true){b+=t(y-b,'S','N')
+while(1>0){b+=t(y-b,'S','N')
 a+=t(x-a,'E','W')
 println}
-def t(d:Int,y:Char,n:Char)=if(d>0)r(y,1)else if(d<0)r(n,-1)else 0
-def r(y:Char,i:Int)={print(y);i}}
+def t(d:Int,y:Char,n:Char)=if(d>0){print(y);1}else if(d<0){print(n);-1}else 0}
