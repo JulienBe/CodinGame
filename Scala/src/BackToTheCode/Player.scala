b2f3c190bf887e33740eb2aed4b401891196c880
JulienBe <bertozzijulien@gmail.com>
2015-09-26 22:14:10 +0200

    Skeleton

--- Scala/src/BackToTheCode/Player.scala
+++ Scala/src/BackToTheCode/Player.scala
@@ -2,28 +2,39 @@ import math._
 import scala.util._
 
 object Player extends App {
-  val opponentcount = readInt // Opponent count
+  val opponentcount = readInt
+  val myPod = new Pod(0, 0, 0)
+  val enemies = new Array[Pod](opponentcount)
+  for (i <- 0 until opponentcount)
+    enemies(i) = new Pod
+  val map: Map = new Map
 
-  // game loop
   while(true) {
     val gameround = readInt
-    // x: Your x position
-    // y: Your y position
-    // backintimeleft: Remaining back in time
-    val Array(x, y, backintimeleft) = for(i <- readLine split " ") yield i.toInt
-    for(i <- 0 until opponentcount) {
-      // opponentx: X position of the opponent
-      // opponenty: Y position of the opponent
-      // opponentbackintimeleft: Remaining back in time of the opponent
-      val Array(opponentx, opponenty, opponentbackintimeleft) = for(i <- readLine split " ") yield i.toInt
-    }
-    for(i <- 0 until 20) {
-      val line = readLine // One line of the map ('.' = free, '0' = you, otherwise the id of the opponent)
-    }
 
-    // Write an action using println
-    // To debug: Console.err.println("Debug messages...")
+    myPod.update(for(i <- readLine split " ") yield i.toInt)
 
-    println("17 10") // action: "x y" to move or "BACK rounds" to go back in time
+    for(i <- 0 until opponentcount)
+      enemies(0).update(for(i <- readLine split " ") yield i.toInt)
+
+    for(i <- 0 until 20)
+      map.update(readLine, i)
+
+     println("17 10") // action: "x y" to move or "BACK rounds" to go back in time
   }
+}
+case class Pod(var x: Int = 0, var y: Int = 0, var backInTimeLeft: Int = 0) {
+  def update(ints: Array[Int]) = {
+    x = ints(0)
+    y = ints(1)
+    backInTimeLeft = ints(2)
+  }
+}
+// One line of the map ('.' = free, '0' = you, otherwise the id of the opponent)
+case class Map(val rep: Array[String] = new Array[String](20)) {
+
+  def update(readLine: String, line: Int) = {
+    rep(line) = readLine
+  }
+
 }
