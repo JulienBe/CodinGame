bb2df03e03aff3918da05c0034560383da36fac5
JulienBe <bertozzijulien@gmail.com>
2015-09-27 11:34:01 +0200

    Skeleton++

--- Scala/src/BackToTheCode/Player.scala
+++ Scala/src/BackToTheCode/Player.scala
@@ -20,9 +20,27 @@ object Player extends App {
     for(i <- 0 until 20)
       map.update(readLine, i)
 
-     println("17 10") // action: "x y" to move or "BACK rounds" to go back in time
+    println("17 10") // action: "x y" to move or "BACK rounds" to go back in time
   }
 }
+
+object Status{
+  val free = '.'.toInt
+  val mine = '0'.toInt
+  val p1 = '1'.toInt
+  val p2 = '2'.toInt
+  val p3 = '3'.toInt
+}
+
+object Builder {
+  def makeGrid(cells: Array[Array[Cell]]) = {
+    for (y <- 0 until cells.length)
+      for (x <- 0 until cells(0).length)
+        cells(y)(x) = new Cell(Status.free, x, y)
+    cells
+  }
+}
+
 case class Pod(var x: Int = 0, var y: Int = 0, var backInTimeLeft: Int = 0) {
   def update(ints: Array[Int]) = {
     x = ints(0)
@@ -30,11 +48,16 @@ case class Pod(var x: Int = 0, var y: Int = 0, var backInTimeLeft: Int = 0) {
     backInTimeLeft = ints(2)
   }
 }
-// One line of the map ('.' = free, '0' = you, otherwise the id of the opponent)
-case class Map(val rep: Array[String] = new Array[String](20)) {
 
-  def update(readLine: String, line: Int) = {
-    rep(line) = readLine
+// One line of the map ('.' = free, '0' = you, otherwise the id of the opponent)
+case class Map(val rep: Array[Array[Cell]] = Builder.makeGrid(Array.ofDim[Cell](20, 35))) {
+
+  def update(readLine: String, lineIndex: Int) = {
+    val line = readLine
+    for (i <- 0 until line.length)
+      rep(lineIndex)(i).status = line.charAt(i)
   }
 
 }
+
+case class Cell(var status: Int, val x: Int, val y: Int)
