ab877570260ca0fea11314114c2a564913fc9e4b
Bertozzi Julien <bertozzijulien@gmail.com>
2015-10-06 17:12:54 +0200

    Create Server.scala

---
+++ Scala/src/Server.scala
@@ -0,0 +1,15 @@
+abstract class Server(clients: List[Client]) {
+
+  def start() = {
+    client.foreach(sendStartData(_))
+    while (!isFinished)
+      act
+    end  
+  }
+  
+  def act
+  def end
+  
+  def sendStartData(client: Client)
+  def isFinished: Boolean
+}
