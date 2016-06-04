d9e24a77f66a44c5d2522d9f9bca941374bd1991
JulienBe <bertozzijulien@gmail.com>
2016-06-04 23:25:40 +0200

    Temperatures short

---
+++ src/Scala/TemperaturesLeastChar.scala
@@ -0,0 +1,10 @@
+package Scala
+object Solution extends App{val n=readInt
+val t=readLine.split(" ")
+var c=5527*n
+if(c>0)t.foreach((s:String)=>c={val i=s.toInt
+if(a(i)<a(c)||i==a(c))i
+else
+c})
+println(c)
+def a(i:Int)=Math.abs(i)}
