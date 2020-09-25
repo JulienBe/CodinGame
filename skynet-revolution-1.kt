fbd54d30a98573ec86e30afcef0f6d85f05d0462
Bertozzi Julien <bertozzijulien@gmail.com>
2020-09-25 22:00:11 +0200

    naive AI

--- skynet-revolution-1.kt
+++ skynet-revolution-1.kt
@@ -8,12 +8,16 @@ fun main(args : Array<String>) {
     val L = input.nextInt() // the number of links
     val E = input.nextInt() // the number of exit gateways
 
+    val nodes = Array<Node>(N) { Node(it) }
     for (i in 0 until L) {
         val N1 = input.nextInt() // N1 and N2 defines a link between these nodes
         val N2 = input.nextInt()
+        nodes[N1].connections.add(N2)
+        nodes[N2].connections.add(N1)
     }
     for (i in 0 until E) {
         val EI = input.nextInt() // the index of a gateway node
+        nodes[EI].exit = true
     }
 
     // game loop
@@ -23,8 +27,22 @@ fun main(args : Array<String>) {
         // Write an action using println()
         // To debug: System.err.println("Debug messages...");
 
+        val nodeA =
+            nodes.firstOrNull { it.exit && it.connections.isNotEmpty() } ?:
+            nodes.first { it.connections.isNotEmpty() }
+        val nodeB = nodes[nodes[nodeA.index].connections[0]]
+        cut(nodeA, nodeB)
+    }
 
-        // Example: 0 1 are the indices of the nodes you wish to sever the link between
-        println("0 1")
+}
+fun cut(a: Node, b: Node) {
+    a.connections.remove(b.index)
+    b.connections.remove(a.index)
+    println("$a $b")
+}
+
+data class Node(val index: Int, var exit: Boolean = false, val connections: MutableList<Int> = mutableListOf<Int>()) {
+    override fun toString(): String {
+        return "$index"
     }
 }
