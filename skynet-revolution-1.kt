7965d1d3bf67394f24f3e98b05ee280dba10867b
Bertozzi Julien <bertozzijulien@gmail.com>
2020-10-17 10:54:02 +0200

    Update skynet-revolution-1.kt

--- skynet-revolution-1.kt
+++ skynet-revolution-1.kt
@@ -1,6 +1,4 @@
 import java.util.*
-import java.io.*
-import java.math.*
 
 fun main(args : Array<String>) {
     val input = Scanner(System.`in`)
@@ -8,40 +6,82 @@ fun main(args : Array<String>) {
     val L = input.nextInt() // the number of links
     val E = input.nextInt() // the number of exit gateways
 
-    val nodes = Array<Node>(N) { Node(it) }
+    val adjacencyList = Array(N) {
+        mutableSetOf<Node>()
+    }
+    val nodes = Array(N) { Node(it) }
+
     for (i in 0 until L) {
         val N1 = input.nextInt() // N1 and N2 defines a link between these nodes
         val N2 = input.nextInt()
-        nodes[N1].connections.add(N2)
-        nodes[N2].connections.add(N1)
-    }
-    for (i in 0 until E) {
-        val EI = input.nextInt() // the index of a gateway node
-        nodes[EI].exit = true
+        adjacencyList[N1].add(nodes[N2])
+        adjacencyList[N2].add(nodes[N1])
     }
 
-    // game loop
+    for (i in 0 until E) {
+        val exit = input.nextInt()
+        nodes[exit].exit = true
+    }
+    val exits: Set<Node> = nodes.filter { it.exit }.toHashSet()
+
+
     while (true) {
         val SI = input.nextInt() // The index of the node on which the Skynet agent is positioned this turn
+        val exitPaths = findExitPaths(nodes[SI], adjacencyList, exits)
 
-        // Write an action using println()
-        // To debug: System.err.println("Debug messages...");
+        exitPaths.forEach {
+            System.err.println(it)
+        }
 
-        val nodeA =
-            nodes.firstOrNull { it.exit && it.connections.isNotEmpty() } ?:
-            nodes.first { it.connections.isNotEmpty() }
-        val nodeB = nodes[nodes[nodeA.index].connections[0]]
-        cut(nodeA, nodeB)
+        cut(exitPaths.first().nodes.elementAt(0), exitPaths.first().nodes.elementAt(1), adjacencyList)
     }
-
 }
-fun cut(a: Node, b: Node) {
-    a.connections.remove(b.index)
-    b.connections.remove(a.index)
+
+data class Path(val origin: Node, val goal: Node, val nodes: MutableSet<Node> = mutableSetOf()) {
+    init {
+        nodes.add(origin)
+    }
+}
+
+fun findExitPaths(origin: Node, adjacencyList: Array<MutableSet<Node>>, exits: Set<Node>): List<Path> {
+    System.err.println("Search exits for ${origin.index}")
+    return exits.map { exit ->
+        travelPathToExit(Path(origin, exit), origin, 0, Array(adjacencyList.size) { false }, exit, adjacencyList)
+    }.sortedBy { it.nodes.size }
+}
+
+fun travelPathToExit(path: Path, origin: Node, currentDepth: Int, visited: Array<Boolean>, goal: Node, adjacencyList: Array<MutableSet<Node>>): Path {
+    if (currentDepth > 10)
+        return path
+
+    visited[origin.index] = true
+    path.nodes.add(origin)
+    val neighbors = adjacencyList[origin.index]
+
+    if (neighbors.any { it == goal }) {
+        path.nodes.add(goal)
+        return path
+    }
+    val toVisitNext = neighbors.filter { !visited[it.index] }
+    neighbors.forEach {
+        visited[it.index] = true
+    }
+    val paths = toVisitNext.map {
+        travelPathToExit(path, it, currentDepth + 1, visited, goal, adjacencyList)
+    }
+    return if (paths.isEmpty())
+        path
+    else
+        paths.first()
+}
+
+fun cut(a: Node, b: Node, adjacencyList: Array<MutableSet<Node>>) {
     println("$a $b")
+    adjacencyList[a.index].remove(b)
+    adjacencyList[b.index].remove(a)
 }
 
-data class Node(val index: Int, var exit: Boolean = false, val connections: MutableList<Int> = mutableListOf<Int>()) {
+data class Node(val index: Int, var exit: Boolean = false, var dstFromExit: Int = Int.MAX_VALUE) {
     override fun toString(): String {
         return "$index"
     }
