385bbc46f17c785507d6b8114c5d372fef594cd3
Julien Bertozzi <bertozzijulien@gmail.com>
2022-11-06 17:04:59 +0100

    line-racing: actually avoid going off the map

--- src/Kotlin/line-racing.kt
+++ src/Kotlin/line-racing.kt
@@ -1,7 +1,7 @@
 import java.util.*
 
 /**
- * In this game your are a program driving the legendary tron light cycle and fighting against other programs on the game grid.
+ * In this game you are a program driving the legendary tron light cycle and fighting against other programs on the game grid.
  *
  * The light cycle moves in straight lines and only turn in 90° angles while leaving a solid light ribbon in its wake.
  * Each cycle and associated ribbon features a different color.
@@ -14,22 +14,26 @@ import java.util.*
  *  The more battles you win, the better your rank will be.
  **/
 fun main(args : Array<String>) {
-    val grid = Grid(20, 30)
+    val grid = Grid(30, 20)
     val input = Scanner(System.`in`)
-
+    val playerHistory = mutableListOf<Array<Player>>()
+    var turn = 0
     // game loop
     while (true) {
         val N = input.nextInt() // total number of players (2 to 4).
         val myId = input.nextInt()
         val players: Array<Player> = Array(N) {
-            val segmentOriginalX = input.nextInt() // starting X coordinate of lightcycle (or -1)
-            val segmentOriginalY = input.nextInt() // starting Y coordinate of lightcycle (or -1)
-            val currentX = input.nextInt()
-            val currentY = input.nextInt()
-            Player(it, Pos(currentX, currentY), Direction.guess(segmentOriginalX, segmentOriginalY, currentX, currentY))
+            val start = Pos(input.nextInt(), input.nextInt())
+            val current = Pos(input.nextInt(), input.nextInt())
+            val previous = playerHistory
+                .getOrElse(turn - 1) { arrayOf() }
+                .getOrElse(it) { Player(myId, current, Direction.NONE) }.pos
+            Player(it, current, Direction.guess(current, previous))
         }
+        playerHistory.add(players)
         players[myId].direction = computeNewDir(grid, players, myId)
         println(players[myId].direction)
+        turn++
     }
 }
 
@@ -38,7 +42,8 @@ fun computeNewDir(grid: Grid, players: Array<Player>, id: Int): Direction {
     val me = players[id]
     val dirsToTry = me.direction.andAdjacent()
     val newDir: Direction? = dirsToTry.firstOrNull {
-        grid.isValid(me.pos + it)
+        val valid = grid.isValid(me.pos + it)
+        valid
     }
     return newDir ?: me.direction
 }
@@ -52,20 +57,21 @@ data class Pos(val x: Int, val y: Int) {
     operator fun plus(direction: Direction): Pos = Pos(x + direction.x, y + direction.y)
 }
 
-enum class Direction(val x: Int, val y: Int) { UP(0, 1), DOWN(0, -1), LEFT(-1, 0), RIGHT(1, 0);
+enum class Direction(val x: Int, val y: Int) {
+    UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0), NONE(0, 0);
+
     fun andAdjacent(): Array<Direction> {
         return when (this) {
             UP, DOWN -> arrayOf(this, LEFT, RIGHT)
             LEFT, RIGHT -> arrayOf(this, UP, DOWN)
+            NONE -> arrayOf(UP, DOWN, LEFT, RIGHT)
         }
     }
 
     companion object {
-        fun guess(segmentOriginalX: Int, segmentOriginalY: Int, currentX: Int, currentY: Int): Direction {
-            val diffX = (segmentOriginalX - currentX).coerceIn(-1, 1)
-            val diffY = (segmentOriginalY - currentY).coerceIn(-1, 1)
-            if (diffX == 0 && diffY == 0)
-                return values().random()
+        fun guess(current: Pos, previous: Pos): Direction {
+            val diffX = (current.x - previous.x).coerceIn(-1, 1)
+            val diffY = (current.y - previous.y).coerceIn(-1, 1)
             return values().first { it.x == diffX && it.y == diffY }
         }
     }
